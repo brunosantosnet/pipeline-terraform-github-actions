baf8aab5bd6fe287f299acefded6d1a267fa228a
Bruno Santos <bruno.santos@zoop.com.br>
2021-12-18 18:27:57 -0300

    Complete...

--- deployment/terraform/environments/prd.tfvars
+++ deployment/terraform/environments/prd.tfvars
@@ -1,6 +1,6 @@
-azs = ["sa-east-1a", "sa-east-1c"]
-cidr = "10.10.0.0/16"
-environment = "prd"
+azs             = ["sa-east-1a", "sa-east-1c"]
+cidr            = "10.10.0.0/16"
+environment     = "prd"
 private-subnets = ["10.10.10.0/24", "10.10.20.0/24"]
-public-subnets = ["10.10.30.0/24", "10.10.40.0/24"]
-region = "sa-east-1"
+public-subnets  = ["10.10.30.0/24", "10.10.40.0/24"]
+region          = "sa-east-1"
--- deployment/terraform/vpc.tf
+++ deployment/terraform/vpc.tf
@@ -2,13 +2,13 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.10.0"
 
-  name = "bbs-${var.environment}-network"
+  name = "pipeline-terraform-github-actions-${var.environment}-network"
   cidr = var.cidr
 
   azs             = var.azs
   private_subnets = var.private-subnets
   public_subnets  = var.public-subnets
 
-  enable_nat_gateway                 = false
+  enable_nat_gateway = false
 
 }
